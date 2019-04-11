6dac27ff8e259a9ffb00e9b4f10db4bd0a43a6d1
Robert Vojta <robert@balena.io>
2019-04-11 09:55:17 +0200

    Rust 2018 edition
    Signed-off-by: Robert Vojta <robert@balena.io>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-#[macro_use]
-extern crate clap;
-
-use clap::{App, Arg};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
 fn main() {
     let matches = App::new(crate_name!())
         .author(crate_authors!())
         .version(crate_version!())
         .about(crate_description!())
-        .arg(
-            Arg::with_name("name")
-                .index(1)
-                .required(true)
-        )
+        .arg(Arg::with_name("name").index(1).required(true))
         .get_matches();
 
     println!("Hello, {}!", matches.value_of("name").unwrap());
