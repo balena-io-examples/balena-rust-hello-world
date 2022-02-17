6d6fd86273f0d3dbae552c1e2b2a96a7f4c4ff3a
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2022-02-17 11:52:18 +0530

    Listen on all interfaces in Docker
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let port = 80;
+
     let index = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file("./views/index.html"));
 
     let public = warp::path("public").and(warp::fs::dir("./views/public"));
     let routes = index.or(public);
-    warp::serve(routes).run(([127, 0, 0, 1], 80)).await;
+    println!("Listenning for requests on port {}", port);
+    warp::serve(routes).run(([0, 0, 0, 0], port)).await;
 }
